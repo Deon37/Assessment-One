e7af21d616a71f6696ef4db398149fccbdbb797f
Deon Miller <165733485+Deon37@users.noreply.github.com>
2025-07-15 13:52:52 +1000

    Part 2 Done

--- dmille27-A1/src/AssignmentOne.java
+++ dmille27-A1/src/AssignmentOne.java
@@ -1,4 +1,4 @@
-class HealthProfessional 
+class HealthProfessional
 {
     private int id;
     private String name;
@@ -6,7 +6,7 @@ class HealthProfessional
 
     public HealthProfessional() {
         this.id = 0;
-        this.name = "Unknown";
+        this.name = "Unknown Name";
         this.department = "General";
     }
 
@@ -23,11 +23,66 @@ class HealthProfessional
     }
 }
 
-public class AssignmentOne 
-{
-    public static void main(String[] args) throws Exception 
+class GeneralPractitioner extends HealthProfessional
+ {
+    private String specialisation;
+
+    public GeneralPractitioner() {
+        super();
+        this.specialisation = "Unknown Specialisation";
+    }
+
+    public GeneralPractitioner(int id, String name, String department, String specialisation) {
+        super(id, name, department);
+        this.specialisation = specialisation;
+    }
+
+    @Override
+    public void PrintDetails() {
+        System.out.println("The health professional details are:");
+        super.PrintDetails();
+        System.out.println("Specialisation: " + this.specialisation);
+        System.out.println("Type: General Practitioner");
+    }
+}
+
+class SpecialistDoctor extends HealthProfessional {
+    private String field;
+
+    public SpecialistDoctor() {
+        super();
+        this.field = "Unknown Specialty";
+    }
+
+    public SpecialistDoctor(int id, String name, String department, String field) {
+        super(id, name, department);
+        this.field = field;
+    }
+
+    @Override
+    public void PrintDetails() {
+        System.out.println("The health professional details are:");
+        super.PrintDetails();
+        System.out.println("Field: " + this.field);
+        System.out.println("Type: Specialist Doctor");
+    }
+}
+
+public class AssignmentOne
+ {
+    public static void main(String[] args) 
     {
-        HealthProfessional hp = new HealthProfessional(1, "Dr. Alice", "General Medicine");
+        HealthProfessional hp = new HealthProfessional(4, "Wren Casement", "General Nursing");
         hp.PrintDetails();
+
+        System.out.println();
+
+        GeneralPractitioner gp = new GeneralPractitioner(2, "Dr. Caitlin Witt", "Family Medicine", "Women's Health");
+        gp.PrintDetails();
+
+        System.out.println();
+
+        SpecialistDoctor sd = new SpecialistDoctor(3, "Dr. Tom Lucena", "Diagnostics", "Human Behaviour & Psychology");
+        sd.PrintDetails();
     }
 }
